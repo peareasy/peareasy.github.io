50fb6e2f9ae995332be283f50aee4244018608e9
Christian <you@example.com>
2021-10-04 17:01:14 +0200

    added button and get players api

--- src/api/api.js
+++ src/api/api.js
@@ -7,3 +7,4 @@ const instance = axios.create({
 export default instance
 
 export { postUUID } from "./requests/postUUID"
+export { getPlayers } from "./requests/getPlayers"
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import { useCookies } from "react-cookie";
 import * as api from "../api/api";
 
 const Home = () => {
   //TODO: find way to get extension id
   const extensionId = "jjkdpohdgeeohccdmbhmecimolaglhkd";
-
   const [cookies, setCookie] = useCookies(["peareasy"]);
+  const [players, setPlayers] = useState([])
+
   useEffect(() => {
     if (!cookies["peareasy"]) {
       api.postUUID().then((uuid: string) => {
@@ -29,11 +30,24 @@ const Home = () => {
     }
   }, [cookies])
 
-  return <div style={{
-    textAlign: "center"
-  }}>
-    {cookies['peareasy']}
-  </div>;
+  const onGetPlayers = () => {
+    api.getPlayers(cookies['peareasy']).then((players) => {
+      setPlayers(players)
+    })
+  }
+
+  let playersJsx: JSX.Element[] = []
+  if (players) {
+    players.map(player => playersJsx.push(<p style={{width: "100%"}}>{player}</p>))
+  }
+
+  return <ul>
+    {<p style={{width: "100%"}}>{cookies['peareasy']}</p> }
+    <button onClick={onGetPlayers}>
+      Get players
+    </button>
+      {playersJsx}
+  </ul>;
 };
 
 export default Home;
