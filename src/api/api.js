3bd82b91c8f4a3278ffcf47faec3a4eda7b70aa7
Christian <you@example.com>
2021-10-18 17:09:06 +0200

    pointing at production url for backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://5fmhq2i8rp.eu-west-1.awsapprunner.com"
 })
 
 export default instance
