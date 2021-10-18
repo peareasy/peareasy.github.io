fb2de14cb18f140284c22235d92677ebf33a50e8
Sebastian <sebastian.a.specht@gmail.com>
2021-10-18 16:50:03 +0200

    added auto deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,18 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import * as api from "../api/api";
 
-
 interface SBC {
-  name: string
+  name: string;
 }
 
 const Home = () => {
   //TODO: find way to get extension id
-<<<<<<< HEAD
   const extensionId = "pcjpfpboackpjobnpeknldjaencjmjlp";
 
-=======
-  const extensionId = "jjkdpohdgeeohccdmbhmecimolaglhkd";
->>>>>>> d1a1880c76a6313361949b80ec1d4677f0185048
   const [cookies, setCookie] = useCookies(["peareasy"]);
-  const [players, setPlayers] = useState([])
-  const [sbcs, setSBCs] = useState<SBC[]>([])
+  const [players, setPlayers] = useState([]);
+  const [sbcs, setSBCs] = useState<SBC[]>([]);
 
   useEffect(() => {
     if (!cookies["peareasy"]) {
@@ -38,60 +33,68 @@ const Home = () => {
     }
   }, [cookies]);
 
-<<<<<<< HEAD
-  return (
-    <div
-      style={{
-        textAlign: "center",
-      }}
-    >
-      {cookies["peareasy"]}
-    </div>
-  );
-=======
   const onGetPlayers = () => {
-    api.getPlayers(cookies['peareasy']).then((players) => {
-      setPlayers(players)
-    })
-  }
+    api.getPlayers(cookies["peareasy"]).then((players) => {
+      setPlayers(players);
+    });
+  };
 
   const onGetSBCs = () => {
     api.getSBCs().then((sbcs) => {
-      setSBCs(sbcs)
-    })
-  }
+      setSBCs(sbcs);
+    });
+  };
 
   const onSolveSBC = (sbc: string) => {
-    api.solveSBC(cookies['peareasy'], sbc).then(res => console.log(res))
-  }
+    api.solveSBC(cookies["peareasy"], sbc).then((res) => console.log(res));
+  };
 
-  let playersJsx: JSX.Element[] = []
+  let playersJsx: JSX.Element[] = [];
   if (players) {
-    players.map(player => playersJsx.push(<p key={player} style={{width: "100%"}}>{player}</p>))
+    players.map((player) =>
+      playersJsx.push(
+        <p key={player} style={{ width: "100%" }}>
+          {player}
+        </p>
+      )
+    );
   }
 
   // For testing purposes
 
-  let sbcsJsx: JSX.Element[] = []
+  let sbcsJsx: JSX.Element[] = [];
   if (sbcs) {
-    sbcs.map(sbc => sbcsJsx.push(<button key={sbc.name} style={{width: "100%", marginTop: "10px"}} onClick={() => onSolveSBC(sbc.name)}>{sbc.name}</button>))
+    sbcs.map((sbc) =>
+      sbcsJsx.push(
+        <button
+          key={sbc.name}
+          style={{ width: "100%", marginTop: "10px" }}
+          onClick={() => onSolveSBC(sbc.name)}
+        >
+          {sbc.name}
+        </button>
+      )
+    );
   }
 
-
-  return <ul style={{width: "50%", margin: "auto"}}>
-    {<p style={{width: "100%"}}>{cookies['peareasy']}</p> }
-    <button onClick={onGetPlayers}>
-      Get players
-    </button>
-    <button onClick={onGetSBCs}>
-      Get SBCs
-    </button>
-    <div style={{height: "600px", overflow: "scroll", backgroundColor: "whitesmoke", marginTop: "10px"}}>
-      {playersJsx}
-    </div>
+  return (
+    <ul style={{ width: "50%", margin: "auto" }}>
+      {<p style={{ width: "100%" }}>{cookies["peareasy"]}</p>}
+      <button onClick={onGetPlayers}>Get players</button>
+      <button onClick={onGetSBCs}>Get SBCs</button>
+      <div
+        style={{
+          height: "600px",
+          overflow: "scroll",
+          backgroundColor: "whitesmoke",
+          marginTop: "10px",
+        }}
+      >
+        {playersJsx}
+      </div>
       {sbcsJsx}
-  </ul>;
->>>>>>> d1a1880c76a6313361949b80ec1d4677f0185048
+    </ul>
+  );
 };
 
 export default Home;
