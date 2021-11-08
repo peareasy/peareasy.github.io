73d0e88b5df6b4db0201fb31ee5013bc6008b109
sasp1 <sebastian.a.specht@gmail.com>
2021-11-08 14:00:08 +0100

    now uses env file for extension id

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,15 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://5fmhq2i8rp.eu-west-1.awsapprunner.com"
-})
+  baseURL: "https://5fmhq2i8rp.eu-west-1.awsapprunner.com",
+  // baseURL: "http://localhost:3000",
+});
 
-export default instance
+export default instance;
 
-export { loginAsAnonymous } from "./requests/loginAsAnonymous"
-export { getPlayers } from "./requests/getPlayers"
-export { getSBCs } from "./requests/getSBCs"
-export { solveSBC } from "./requests/solveSBC"
-export { signIn } from "./requests/signIn"
-export { signUp } from "./requests/signUp"
+export { loginAsAnonymous } from "./requests/loginAsAnonymous";
+export { getPlayers } from "./requests/getPlayers";
+export { getSBCs } from "./requests/getSBCs";
+export { solveSBC } from "./requests/solveSBC";
+export { signIn } from "./requests/signIn";
+export { signUp } from "./requests/signUp";
--- src/api/requests/getPlayers.js
+++ src/api/requests/getPlayers.js
@@ -1,11 +1,14 @@
-import axios from "../api"
+import axios from "../api";
 
 export const getPlayers = (uuid) => {
-  return axios.get('/players', {headers: { "x-auth-token": uuid }}).then(response => {
-    let players = [];
-    if (response.data) {
-      players = response.data
-    }
-    return players
-  })
-}
+  console.log(uuid);
+  return axios
+    .get("/players", { headers: { "x-auth-token": uuid } })
+    .then((response) => {
+      let players = [];
+      if (response.data) {
+        players = response.data;
+      }
+      return players;
+    });
+};
