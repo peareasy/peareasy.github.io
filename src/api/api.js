e1d91e075a8986986f53c8c1e88ac22a6a6c01fd
sasp1 <sebastian.a.specht@gmail.com>
2022-03-21 21:15:29 +0100

    made backend url dynamic with env var

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://5fmhq2i8rp.eu-west-1.awsapprunner.com",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 export default instance;
