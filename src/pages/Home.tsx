c99121ce0e0709daab7eaa332c4d99059d86a01b
Christian <christianhjelmslund@gmail.com>
2022-03-28 09:28:40 +0200

    spelling mistake

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -232,7 +232,7 @@ const Home = () => {
 
   const nonChromeView = (<div className="space-y-8">
     <h1 className="text-3xl font-bold mx-auto h-4/5 overflow-y-auto">
-      Oh no, you are on not using Chrome! 😔
+      Oh no, you are not using Chrome! 😔
     </h1>
     <p>In order for you to use our Chrome extension to import your players, you have to use this site from a Chrome browser!</p>
   </div>)
