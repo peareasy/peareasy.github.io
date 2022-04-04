fe1ad48532c6f3f3dccbf93a13f77c250932c454
Christian <christianhjelmslund@gmail.com>
2022-04-04 16:13:11 +0200

    changed chrome view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -319,15 +319,15 @@ const Home = () => {
     <h1 className="text-xl">
       You need our Chrome extension to import your players
     </h1>
+    <p className="text-xl">
+      Please refresh the application once you have downloaded the extension 🔄
+    </p>
     <img alt={"img"} className="m-auto w-1/4" src={process.env.PUBLIC_URL+'/chrome.svg'}/>
     <PrimaryButton title={"Download Extension"} icon={openLinkIcon} onClick={() =>
       window.open(
         'https://chrome.google.com/webstore/detail/auto-sbc/mchecdiinfipdfihkoebfbpfnllbllhc?hl=en-GB',
         '_blank'
       )}/>
-    <p className="text-l">
-      Please refresh the application once you have downloaded the extension 🔄
-    </p>
   </div>
 
   const isChrome = !!window.chrome
