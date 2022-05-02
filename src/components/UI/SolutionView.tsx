6163be58041ffff2a99ff1c6062284dc4aa8e87a
sasp1 <sebastian.a.specht@gmail.com>
2022-05-02 20:47:21 +0200

    Update for solution view

--- src/components/UI/SolutionView.tsx
+++ src/components/UI/SolutionView.tsx
@@ -3,6 +3,7 @@ import { CopyButton } from "./CopyButton";
 import {Solution} from "../../interfaces/Solution";
 import {SBC} from "../../interfaces/SBC";
 import {copied} from "./icons";
+import React from "react";
 
 type SolutionViewProps = {
   players: Player[]
@@ -51,23 +52,27 @@ const SolutionView = ({players, solution, sbc}: SolutionViewProps) => {
       <div className="flex flex-col gap-y-4">
         {formationWithPlayers.reverse()}
       </div>
-      <div className="text-secondary text-center font-light rounded-l bg-gray-800 flex flex-col gap-y-4 w-72">
-        <h1 className="font-light mt-4 text-xl">Constraints</h1>
-        <p className="text-m flex flex-row ml-2"><span className="pr-1">{copied}</span>Rating: {sbc.min_rating} ({solution.rating})</p>
-        <p className="text-m flex flex-row ml-2"><span className="pr-1">{copied}</span>Chem: {sbc.min_chemistry} ({solution.chem})</p>
-        <p className="text-m flex flex-row mx-auto">Formation: {solution.formation}</p>
-        <p className="text-m flex flex-row mx-auto">Cost: {solution.cost}</p>
-        <p className="border border-primary-300"/>
-        <div className="flex flex-col m-auto gap-y-4">
-          <div className="flex flex-row mx-auto">
-            <div className="w-6 m-auto">
-              <img className="w-4" alt="loyalty" src={process.env.PUBLIC_URL + '/loyalty.png'}/>
+
+      <div className="space-y-4 text-center">
+        <div className="text-secondary text-center font-light rounded-l bg-gray-800 flex flex-col gap-y-4 w-72 pb-4">
+          <h1 className="font-light mt-4 text-xl">Constraints</h1>
+          <p className="text-m flex flex-row ml-2"><span className="pr-1">{copied}</span>Rating: {sbc.min_rating} ({solution.rating})</p>
+          <p className="text-m flex flex-row ml-2"><span className="pr-1">{copied}</span>Chem: {sbc.min_chemistry} ({solution.chem})</p>
+          <p className="text-m flex flex-row mx-auto">Formation: {solution.formation}</p>
+          <p className="text-m flex flex-row mx-auto">Cost: {solution.cost}</p>
+        </div>
+        <div className="text-secondary text-center font-light rounded-l bg-gray-800 flex flex-col gap-y-4 w-72 pt-4">
+          <div className="flex flex-col m-auto gap-y-4">
+            <div className="flex flex-row mx-auto">
+              <div className="w-6 m-auto">
+                <img className="w-4" alt="loyalty" src={process.env.PUBLIC_URL + '/loyalty.png'}/>
+              </div>
+              <span>Loyalty</span>
             </div>
-            <span>Loyalty</span>
-          </div>
-          <div className="flex flex-row mx-auto mb-4">
-            <img className="w-6" alt="untradeable" src={process.env.PUBLIC_URL + '/untradeable.png'}/>
-            <span>Untradable</span>
+            <div className="flex flex-row mx-auto mb-4">
+              <img className="w-6" alt="untradeable" src={process.env.PUBLIC_URL + '/untradeable.png'}/>
+              <span>Untradable</span>
+            </div>
           </div>
         </div>
       </div>
