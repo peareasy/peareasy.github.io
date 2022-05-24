0d59dec5e2448b4445b80798ae8646febba61089
Christian <christianhjelmslund@gmail.com>
2022-05-24 17:23:40 +0200

    logging out user in client no matter what

--- src/api/privateRequests/logout.js
+++ src/api/privateRequests/logout.js
@@ -1,12 +1,11 @@
 import axios from "../privateApi";
 
 export const logout = () => {
-  return axios.post("/auth/logout").then(res => {
-    if (res.status === 200) {
-      localStorage.removeItem("access_token")
-      localStorage.removeItem("refresh_token")
-      localStorage.removeItem("name")
-      localStorage.removeItem("email")
-    }
-  }).catch(err => err)
+  // TODO: think about whether client should be logged out on client only if
+  // logging out on server is possible or not?
+  localStorage.removeItem("access_token")
+  localStorage.removeItem("refresh_token")
+  localStorage.removeItem("name")
+  localStorage.removeItem("email")
+  return axios.post("/auth/logout").then().catch(err => err)
 };
