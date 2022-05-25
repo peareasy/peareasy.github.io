de5fb7989347b62f0911698113ea4d058ca18a29
Christian <christianhjelmslund@gmail.com>
2022-05-25 20:15:01 +0200

    logout updated

--- src/api/privateRequests/logout.js
+++ src/api/privateRequests/logout.js
@@ -3,9 +3,20 @@ import axios from "../privateApi";
 export const logout = () => {
   // TODO: think about whether client should be logged out on client only if
   // logging out on server is possible or not?
+
+  return axios.post("/auth/logout").then(_ => {
+    removeUserFromLocalStorage()
+    }
+  ).catch(_ => {
+    removeUserFromLocalStorage()
+    }
+  )
+};
+
+const removeUserFromLocalStorage = () => {
   localStorage.removeItem("access_token")
   localStorage.removeItem("refresh_token")
   localStorage.removeItem("name")
   localStorage.removeItem("email")
-  return axios.post("/auth/logout").then().catch(err => err)
-};
+}
+
