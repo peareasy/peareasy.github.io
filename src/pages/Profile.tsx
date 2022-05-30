62299fbc4020fba0f3b479b0e803c8434871aa04
Christian Hjelmslund <christianhjelmslund@gmail.com>
2022-05-31 01:25:05 +0200

    updated profile page (#53)

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,9 +2,9 @@ import {PrimaryButton} from "../components/UI/Button";
 import {useNavigate} from "react-router";
 import {useDispatch, useSelector} from 'react-redux';
 import {getUserSelector, logoutUser} from '../redux/user/userSlice';
-import { APIStatus } from '../enums/APIStatus';
 import Spinner from "../components/UI/Spinner/Spinner";
 import {AppDispatch} from "../redux/store";
+import {NavLink} from "react-router-dom";
 
 type LogoutProps = {
   setLogin: (isLoggedIn: boolean) => void;
@@ -21,15 +21,32 @@ const Profile = ({setLogin}: LogoutProps) => {
       navigate('/')
     })
   }
-  return <div className='container mx-auto w-1/3 flex font-light flex-col gap-y-4 p-8 bg-gray-900 rounded text-secondary'>
-    {user.status !== APIStatus.FULFILLED ? <Spinner/>: <>
+  return <div className='container mx-auto w-1/3 flex font-light flex-col gap-y-4 p-8 bg-gray-900 rounded'>
+    {!user.data ? <Spinner/>: <>
     <div>
-        <h1 className='text-l'>Hi, {user.data?.name}</h1>
+        <h1 className='text-xl text-secondary'>Hi, {user.data?.name} 👋</h1>
       </div>
-      <div className={'text-sm text-gray-300'}>
-        This is your email: {user.data?.email}
+      <div className={'text-m text-gray-200 font-bold'}>
+        📧 Email
       </div>
-      <PrimaryButton onClick={() => logout()} title={'Logout'}/> </>}
+      <div className={'text-m text-gray-300'}>
+        {user.data?.email}
+      </div>
+      <div className={'text-m text-gray-200 font-bold'}>
+        💰 Current subscription
+      </div>
+      <div className={'text-m text-gray-300'}>
+        Free tier. You can upgrade {<NavLink to={'/subscription'}>here!</NavLink>}
+      </div>
+      <br/>
+      <div className={'text-m text-gray-300'}>
+        In order to <span className={'text-error-600 font-bold'}>delete</span> your account, please {<NavLink to={'/contact'}>contact us</NavLink>} and we will
+        do it as soon as possible
+      </div>
+      <div className={'m-auto'}>
+        <PrimaryButton onClick={() => logout()} title={'Logout'}/>
+      </div>
+    </>}
     </div>
 }
 export default Profile
