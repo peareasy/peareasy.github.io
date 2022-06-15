89f259cbc3c12ea411ee91cdcb421cfa598ca0bb
Christian <christianhjelmslund@gmail.com>
2022-06-15 19:38:38 +0200

    remove email header if null

--- src/api/publicRequests/solveSBC.js
+++ src/api/publicRequests/solveSBC.js
@@ -1,12 +1,16 @@
 import axios from "../publicApi"
 
 export const solveSBC = (sbc, email) => {
+  let headers
+  if (email) {
+    headers = {
+      email: email
+    }
+  }
   return axios.post('/sbcs', {
     name: sbc,
   }, {
-    headers: {
-      email: email
-    }
+    headers
   }).then(response => {
     if (response.data) {
       return response.data
