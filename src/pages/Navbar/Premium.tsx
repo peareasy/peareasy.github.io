d186f1f339624442864fe441587ab95d1781100f
sasp1 <sebastian.a.specht@gmail.com>
2022-07-04 12:30:57 +0200

    Updated price id and removed unused files

--- src/pages/Navbar/Premium.tsx
+++ src/pages/Navbar/Premium.tsx
@@ -2,13 +2,12 @@ import { useCookies } from "react-cookie"
 import { SubscriptionPlan } from "../../components/UI/Products/SubscriptionPlan"
 const Premium = () => {
   const [{userId}] = useCookies(["userId"]);
-  
     return (
         <main className={'text-secondary text-center m-auto relative z-10'}>
             <div className='flex flex-row'>
                 <SubscriptionPlan 
                   name={'Premium'}
-                  priceId={'price_1KxW36ATqmUvIGSp9HaaKJ1W'}
+                  priceId={process.env.REACT_APP_STRIPE_PRICE_ID_PREMIUM}
                   features={['All SBCs', 'Standard Solver']}
                   userId={userId}
                 />
